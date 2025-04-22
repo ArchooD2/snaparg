9b8cddc4eb893948621afda73ae2d42b8e6dab5b
PJSans <archood2@gmail.com>
2025-04-22 14:17:57 -0700

    Bump version to 0.1.2 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='snaparg',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='A typo-tolerant CLI parser wrapper for argparse',
     author='PJ Hayes',
