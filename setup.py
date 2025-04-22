5b9da5d089dfa741eb7d4ed639a77f808388e5ec
Paper <41011272+ArchooD2@users.noreply.github.com>
2025-04-22 12:00:35 -0700

    Update setup.py
    Contact Info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     description='A typo-tolerant CLI parser wrapper for argparse',
     author='PJ Hayes',
-    author_email='you@example.com',
+    author_email='archood2next@gmail.com',
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
